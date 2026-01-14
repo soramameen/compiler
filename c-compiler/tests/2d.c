09b826b08d248a58dcc152194b66158af7dd3eed
中嶋空偉 <sorai.s.nakajima@gmail.com>
2026-01-14 16:02:45 +0900

    テストケース

---
+++ c-compiler/tests/2d.c
@@ -0,0 +1,36 @@
+array matrix1[2][2];
+array matrix2[2][2];
+array matrix3[2][2];
+define i;
+define j;
+define k;
+matrix1[0][0] = 1;
+matrix1[0][1] = 2;
+matrix1[1][0] = 3;
+matrix1[1][1] = 4;
+matrix2[0][0] = 5;
+matrix2[0][1] = 6;
+matrix2[1][0] = 7;
+matrix2[1][1] = 8;
+i=0;
+while(i<2){
+    j=0;
+    while(j<2){
+        matrix3[i][j] = 0;
+        j++;
+    }
+    i++;
+}
+i=0;
+while(i<2){
+    j=0;
+    while(j<2){
+        k=0;
+        while(k<2){
+            matrix3[i][j] = matrix3[i][j] + matrix1[i][k] * matrix2[k][j];
+            k++;
+        }
+        j++;
+    }
+    i++;
+}
